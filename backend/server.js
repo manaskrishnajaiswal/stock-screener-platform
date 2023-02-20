6db128bf6eaeebda456c16b094f89434f6aa1ee4
manas5996 <manas5996@gmail.com>
2023-02-20 19:20:51 +0530

    code commit!

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,8 @@ import path from "path";
 import colors from "colors";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
-
-connectDB();
 
 const app = express();
 if (process.env.NODE_ENV === "development") {
@@ -20,7 +13,7 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json());
 
-app.use("/api/products", productRoutes);
+app.use("/api/yahoo-finance", screenerRoutes);
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
