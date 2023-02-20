b89612bebcf92a055b2211dd328c8ad99fc6e79a
manas5996 <manas5996@gmail.com>
2023-02-20 19:30:37 +0530

    code commit!

--- backend/routes/screenerRoutes.js
+++ backend/routes/screenerRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
+const router = express.Router();
 import { getStockData } from "../controllers/screenerControlers.js";
 
 // @desc    Get stock data
 // @route   GET /api/yahoo-finance
 // @access  Private
-router.route("/").get(verifyToken, getStockData);
+router.route("/").get(getStockData);
 
 export default router;
